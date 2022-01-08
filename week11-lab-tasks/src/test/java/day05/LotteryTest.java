eae5cd7d55a6a51432ac1eda858d15140ac13cd4
anitaszoke <szanita1735@gmail.com>
2022-01-08 23:16:06 +0100

    Week11 - Day05 - Lottery

--- week11-lab-tasks/src/test/java/day05/LotteryTest.java
+++ week11-lab-tasks/src/test/java/day05/LotteryTest.java
@@ -11,6 +11,11 @@ class LotteryTest {
         Lottery lottery = new Lottery(90,5);
 
         assertEquals(5,lottery.startLottery().size());
+        System.out.println(lottery.startLottery());
+
+        for (int i=0; i < lottery.startLottery().size(); i++) {
+            assertFalse(lottery.startLottery().subList(i+1,lottery.startLottery().size()).contains(lottery.startLottery().get(i)));
+        }
     }
 
 }
